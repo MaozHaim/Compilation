0a91f85381804a227b52c8b330e57b87518ac98d
noa.goldenberg <noa.goldenberg@wiliot.com>
2025-12-24 19:56:32 +0200

    temp

--- ex4/src/temp/Temp.java
+++ ex4/src/temp/Temp.java
@@ -14,14 +14,24 @@ package temp;
 public class Temp
 {
 	private int serial=0;
+	private static int nextSerialNum = 0;
 	
 	public Temp(int serial)
 	{
 		this.serial = serial;
 	}
+
+	public Temp() {
+		this.serial = nextSerialNum++;
+	}
 	
 	public int getSerialNumber()
 	{
 		return serial;
+	}
+
+	@Override
+	public String toString() {
+		return "t" + serial;
 	}
 }
