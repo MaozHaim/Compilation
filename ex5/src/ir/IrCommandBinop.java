95f896c34590b2bec6842c15c28be9636461c660
Maoz1005 <maozhaim@mail.tau.ac.il>
2026-03-03 16:13:50 +0200

    Interval 3 - IrCommandBinop

--- ex5/src/ir/IrCommandBinop.java
+++ ex5/src/ir/IrCommandBinop.java
@@ -2,10 +2,13 @@ package ir;
 
 import temp.Temp;
 
+import java.util.Set;
+
 public abstract class IrCommandBinop extends IrCommand {
     public Temp t1;
     public Temp t2;
     public Temp dst;
+
 
     public IrCommandBinop(Temp dst,Temp t1,Temp t2) {
         this.dst = dst;
@@ -13,8 +16,27 @@ public abstract class IrCommandBinop extends IrCommand {
         this.t2 = t2;
     }
 
+
     @Override
     public String toString() {
         return dst + " := " + t1 + " BINOP " + t2;
+    }
+
+
+    @Override
+    public void calcOut(Set<Integer> inSet) {
+        // In binop, we have: dst := t1 op t2
+        // so, we need to remove dst, and add t1 and t2 to the updated out set
+        inSet.remove(dst.getSerialNumber());
+        inSet.add(t1.getSerialNumber());
+        inSet.add(t2.getSerialNumber());
+    }
+
+
+    @Override
+    public void addTemps(Set<Temp> temps) {
+        temps.add(dst);
+        temps.add(t1);
+        temps.add(t2);
     }
 }
