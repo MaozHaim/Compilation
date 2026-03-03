c9d0b4082ea7dd87ff8dbcc8548f62b55d22ef6a
Maoz1005 <maozhaim@mail.tau.ac.il>
2026-03-03 14:20:28 +0200

    Interval 3 - IrCommand

--- ex5/src/ir/IrCommand.java
+++ ex5/src/ir/IrCommand.java
@@ -1,15 +1,46 @@
 package ir;
 
+import temp.Temp;
+
+import java.util.Set;
+
 public abstract class IrCommand {
 	protected static int labelCounter = 0; /* Label Factory */
 	private static int index_counter = 0;
 	public int index;
 
+
 	public IrCommand() { index = index_counter++; }
 
 
-	public static String getFreshLabel(String msg)
-	{
+	public static String getFreshLabel(String msg) {
 		return String.format("Label_%d_%s", labelCounter++, msg);
 	}
+
+
+	@Override
+	public String toString() {
+		return "PLACEHOLDER: " + super.toString();
+	}
+
+
+	public String toString(String log) {
+		return toString(log, "");
+	}
+
+
+	public String toString(String log, String metadata) {
+		return String.format("%-32s %-32s", log, metadata);
+	}
+
+
+	// Default implementation
+	public void calcOut(Set<Integer> inSet) {}
+
+
+	// Default implementation
+	public void addTemps(Set<Temp> temps) {}
+
+
+	public abstract void MIPSme();
 }
