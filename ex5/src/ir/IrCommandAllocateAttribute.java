200da9bbdadc90f6fa20ae995f2c4fabd907f5ca
Maoz1005 <maozhaim@mail.tau.ac.il>
2026-03-03 14:37:55 +0200

    Interval 3 - IrCommandAllocateAttribute

---
+++ ex5/src/ir/IrCommandAllocateAttribute.java
@@ -0,0 +1,22 @@
+package ir;
+
+public class IrCommandAllocateAttribute extends IrCommand {
+    String varName;
+    String className;
+    int offset;
+
+    public IrCommandAllocateAttribute(String var_name, String className, int offset) {
+        this.varName = var_name;
+        this.className = className;
+        this.offset = offset;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString("Attribute Declaration",
+                "name: " + varName + ", Class: " + className + ", offset: " + offset);
+    }
+
+    @Override
+    public void MIPSme() {}
+}
