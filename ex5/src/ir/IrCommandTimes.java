7746e21f70d1cece9c2ca2d7039fe3add4492352
Maoz1005 <maozhaim@mail.tau.ac.il>
2026-03-03 16:47:56 +0200

    Interval 3 - IrCommandTimes

--- ex5/src/ir/IrCommandTimes.java
+++ ex5/src/ir/IrCommandTimes.java
@@ -1,5 +1,6 @@
 package ir;
 
+import mips.MipsGenerator;
 import temp.*;
 
 public class IrCommandTimes extends IrCommandBinop {
@@ -7,7 +8,20 @@ public class IrCommandTimes extends IrCommandBinop {
     public Temp t2;
     public Temp dst;
 
+
     public IrCommandTimes(Temp dst, Temp t1, Temp t2) {
         super(dst, t1, t2);
+    }
+
+
+    @Override
+    public String toString() {
+        return dst + " := " + t1 + " * " + t2;
+    }
+
+
+    @Override
+    public void MIPSme() {
+        MipsGenerator.getInstance().mul(dst,t1,t2);
     }
 }
