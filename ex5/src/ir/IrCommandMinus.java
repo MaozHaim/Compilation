f9f5786565f137c3357eb0e9863e76c71eada851
Maoz1005 <maozhaim@mail.tau.ac.il>
2026-03-03 16:48:53 +0200

    Interval 3 - IrCommandMinus

--- ex5/src/ir/IrCommandMinus.java
+++ ex5/src/ir/IrCommandMinus.java
@@ -1,13 +1,27 @@
 package ir;
 
-import temp.*;
+import mips.MipsGenerator;
+import temp.Temp;
 
 public class IrCommandMinus extends IrCommandBinop {
     public Temp t1;
     public Temp t2;
     public Temp dst;
 
+
     public IrCommandMinus(Temp dst, Temp t1, Temp t2) {
         super(dst, t1, t2);
+    }
+
+
+    @Override
+    public String toString() {
+        return dst + " := " + t1 + " - " + t2;
+    }
+
+
+    @Override
+    public void MIPSme() {
+        MipsGenerator.getInstance().sub(dst, t1, t2);
     }
 }
