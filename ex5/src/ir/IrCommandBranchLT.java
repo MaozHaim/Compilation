56f00b91068846afb9f1b9237c67514bb23b4f47
Maoz1005 <maozhaim@mail.tau.ac.il>
2026-03-04 11:52:20 +0200

    Interval 3 - IrCommandBranchLT

---
+++ ex5/src/ir/IrCommandBranchLT.java
@@ -0,0 +1,53 @@
+package ir;
+
+import mips.MipsGenerator;
+import temp.Temp;
+
+import java.util.Set;
+
+public class IrCommandBranchLT extends IrCommandJumpType {
+    private Temp t1;
+    private Temp t2;
+    private String label;
+
+
+    public IrCommandBranchLT(Temp t1, Temp t2, String label, boolean ignoreCFG) {
+        super(label, ignoreCFG);
+        this.t1 = t1;
+        this.t2 = t2;
+        this.label = label;
+    }
+
+
+    public IrCommandBranchLT(Temp t1, Temp t2, String label) {
+        this(t1, t2, label, false);
+    }
+
+
+    @Override
+    public String toString() {
+        return super.toString() + " if " + t1 + " < " + t2;
+    }
+
+
+    @Override
+    public void calcOut(Set<Integer> inSet) {
+        // In branchLT, we have: if (t1 < t2) goto label
+        // so, we only need to add t1 and t2 to the updated out set
+        inSet.add(t1.getSerialNumber());
+        inSet.add(t2.getSerialNumber());
+    }
+
+
+    @Override
+    public void addTemps(Set<Temp> temps) {
+        temps.add(t1);
+        temps.add(t2);
+    }
+
+
+    @Override
+    public void MIPSme() {
+        MipsGenerator.getInstance().blt(t1, t2, label);
+    }
+}
