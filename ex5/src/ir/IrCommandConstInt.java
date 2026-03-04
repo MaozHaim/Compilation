fe7e0ff98d343aed5eefc95b32e1a60623c528c1
Maoz1005 <maozhaim@mail.tau.ac.il>
2026-03-04 12:05:34 +0200

    Interval 3 - IrCommandConstInt

--- ex5/src/ir/IrCommandConstInt.java
+++ ex5/src/ir/IrCommandConstInt.java
@@ -1,18 +1,41 @@
 package ir;
 
+import mips.MipsGenerator;
 import temp.Temp;
+
+import java.util.Set;
 
 public class IrCommandConstInt extends IrCommand{
     public Temp t;
     int value;
+
 
     public IrCommandConstInt(Temp t,int value) {
         this.t = t;
         this.value = value;
     }
 
+
     @Override
     public String toString() {
         return t + " := constval " + value;
+    }
+
+
+    @Override
+    public void calcOut(Set<Integer> inSet) {
+        // here, we do t := value, so we need to remove t from the updated out set
+        inSet.remove(t.getSerialNumber());
+    }
+
+
+    @Override
+    public void addTemps(Set<Temp> temps) {
+        temps.add(t);
+    }
+
+
+    public void MIPSme() {
+        MipsGenerator.getInstance().li(t, value);
     }
 }
