6bdc809d3ba11b4ce6bb6eb1161fc7642e345e04
Maoz1005 <maozhaim@mail.tau.ac.il>
2026-03-04 15:32:15 +0200

    Interval 3 - IrCommandStore

--- ex5/src/ir/IrCommandStore.java
+++ ex5/src/ir/IrCommandStore.java
@@ -1,18 +1,48 @@
 package ir;
 
+import mips.MipsGenerator;
 import temp.Temp;
 
-public class IrCommandStore extends IrCommand{
-    public String varName;
-    public Temp src;
+import java.util.Set;
 
-    public IrCommandStore(String var_name, Temp src) {
+// Stores the value at src to the address at dst
+public class IrCommandStore extends IrCommand{
+    Temp src;
+    Temp dst;
+    int offset;
+
+
+    public IrCommandStore(Temp src, Temp dst, int offset) {
         this.src = src;
-        this.varName = var_name;
+        this.dst = dst;
+        this.offset = offset;
     }
+
+
+    @Override
+    public void calcOut(Set<Integer> inSet) {
+        // here, we do dst[offset] := src
+        // so, add src and dst to the updated out set
+        inSet.add(src.getSerialNumber());
+        inSet.add(dst.getSerialNumber());
+    }
+
+
+    @Override
+    public void addTemps(Set<Temp> temps) {
+        temps.add(src);
+        temps.add(dst);
+    }
+
+
+    @Override
+    public void MIPSme() {
+        MipsGenerator.getInstance().sw(src, dst, offset);
+    }
+
 
     @Override
     public String toString() {
-        return varName +  " := " + src;
+        return super.toString(String.format("t%d[%d] := t%d", dst.getSerialNumber(), offset, src.getSerialNumber()));
     }
 }
