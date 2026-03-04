135ddf73a4a5c503864376eda9c5815ca9e7a8d4
Maoz1005 <maozhaim@mail.tau.ac.il>
2026-03-04 11:54:19 +0200

    Interval 3 - IrCommandBranchLTZ

---
+++ ex5/src/ir/IrCommandBranchLTZ.java
@@ -0,0 +1,46 @@
+package ir;
+
+import mips.MipsGenerator;
+import temp.Temp;
+
+import java.util.Set;
+
+public class IrCommandBranchLTZ extends IrCommandJumpType {
+    Temp t1;
+
+
+    public IrCommandBranchLTZ(Temp t1, String label_name) {
+        super(label_name);
+        this.t1 = t1;
+    }
+
+
+    public IrCommandBranchLTZ(Temp t1, String label_name,  boolean ignoreCFG) {
+        super(label_name, ignoreCFG);
+        this.t1 = t1;
+    }
+
+
+    @Override
+    public void calcOut(Set<Integer> inSet) {
+        inSet.add(t1.getSerialNumber());
+    }
+
+
+    @Override
+    public void addTemps(Set<Temp> temps) {
+        temps.add(t1);
+    }
+
+
+    @Override
+    public String toString() {
+        return super.toString() + " if "  + t1 + " < 0";
+    }
+
+
+    @Override
+    public void MIPSme() {
+        MipsGenerator.getInstance().bltz(t1, label_name);
+    }
+}
